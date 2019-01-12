9aaad11c699393d47e69424c62b2134e543b3766
rebecalvarez <rebeca.dodero@gmail.com>
2019-01-12 12:39:30 -0600

    variables

--- server.js
+++ server.js
@@ -7,3 +7,11 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+// variables:
+var reservations = [];
+// $('#reserve_name').val("");
+// $('#reserve_phone').val("");
+// $('#reserve_email').val("");
+// $('#reserve_uniqueID').val("");
