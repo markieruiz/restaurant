c11c2321e8085893a6efc826e54f27d6af8c1a67
Markie Ruiz <markieruiz@Markies-MacBook-Pro.local>
2019-01-12 11:41:38 -0600

    routes

--- server.js
+++ server.js
@@ -7,3 +7,33 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.post("/api/reserve", function(req, res) {
+
+  var newReservation = req.body;
+
+  newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newReservation);
+
+  reserve.push(newReservation);
+
+  res.json(newReservation);
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
